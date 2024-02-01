5352cae1ef727f14d2737ecf110c7762e2331edc
Axhu99 <xhuliodreni@icloud.com>
2024-02-01 22:06:36 +0100

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  name: 'DC',
+  components: { AppHeader }
+};
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+  <main>
+    -> Content goes here
+  </main>
+  <footer>
+    <section class="footer-top">
+
+    </section>
+    <section class="footer-bot">
+      <button>SIGN-UP NOW</button>
+      <div class="social">
+
+      </div>
+    </section>
+  </footer>
+</template>
 
 <style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <header>
+        <nav class="navbar">
+            <figure>
+                <img src="../../public/img/dc-logo.png" alt="">
+            </figure>
+            <ul class="navbar-ul">
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style>
+.navbar {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+
+.navbar-ul {
+    display: flex;
+    gap: 10px;
+}
+
+li {
+    list-style-type: none;
+}
+
+a {
+    text-decoration: none;
+
+}
+</style>
