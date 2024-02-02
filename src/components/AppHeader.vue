2701c3c425c175b1cadac1a35d727dca0317eba7
Axhu99 <xhuliodreni@icloud.com>
2024-02-02 16:45:18 +0100

    dinamic navabar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,16 +14,7 @@ export default {
                 <img src="../../public/img/dc-logo.png" alt="">
             </figure>
             <ul class="navbar-ul">
-                <li><a :href='url'>{{ text }}</a></li>
-                <li><a href="#">COMICS</a></li>
-                <li><a href="#">MOVIES</a></li>
-                <li><a href="#">TV</a></li>
-                <li><a href="#">GAMES</a></li>
-                <li><a href="#">COLLECTIBLES</a></li>
-                <li><a href="#">VIDEOS</a></li>
-                <li><a href="#">FANS</a></li>
-                <li><a href="#">NEWS</a></li>
-                <li><a href="#">SHOP</a></li>
+                <li v-for="navbarLink in navbarLinks"><a :href='navbarLink.url'>{{ navbarLink.text }}</a></li>
             </ul>
         </nav>
     </header>
